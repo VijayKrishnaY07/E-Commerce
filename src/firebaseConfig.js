96a71886111721758e4b32d5c00ea84267b827c4
Vijay Krishna <vijaykrishnay07@gmail.com>
2025-01-25 01:30:14 -0800

    removed dotenv import

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,7 +8,6 @@ import {
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 // Firebase Configuration
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -18,8 +17,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_MEASUREMENT_ID,
 };
-
-export default firebaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -74,7 +71,6 @@ const loadFromFirebase = async (collection, userEmail) => {
     if (docSnap.exists()) {
       console.log(
         `📦 Data loaded from ${collection} for:`,
-
         userEmail,
         docSnap.data()
       );
