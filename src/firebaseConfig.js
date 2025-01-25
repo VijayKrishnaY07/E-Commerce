891a37a0eba9d8f2ad30a2ccb52e3deb36127dee
Vijay Krishna <vijaykrishnay07@gmail.com>
2025-01-25 00:59:10 -0800

    Final fixes and run build

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,15 +8,19 @@ import {
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 // Firebase Configuration
+
 const firebaseConfig = {
-  apiKey: "xxx",
-  authDomain: "xxx",
-  projectId: "xxx",
-  storageBucket: "xxx",
-  messagingSenderId: "xxx",
-  appId: "xxx",
-  measurementId: "xxx",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_MEASUREMENT_ID,
 };
+
+export default firebaseConfig;
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
